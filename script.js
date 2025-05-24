563b02ce4d3586af20286f229bfb44e16a4b5d17
Mohamed <mossmoss4000@gmail.com>
2025-05-24 23:13:42 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toggleMenu(e) {
-    e.preventDefault(); // empêche un comportement par défaut si nécessaire
-    window.location.href = "menu.html"; // redirige vers menu.html
+    e.preventDefault(); 
+    window.location.href = "menu.html"; 
 }
 
-// On attend que le DOM soit chargé pour attacher le listener
+
 document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.getElementById("openMenu");
     if (menuButton) {
